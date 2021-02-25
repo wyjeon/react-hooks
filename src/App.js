bb37335b0f42aa9dfea3f9abbdf52be7ddc8dc42
wyjeon <wyjeon.info@gmail.com>
2021-02-25 13:48:07 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { useState } from 'react';
-import Counter from './Counter';
-import Info from './Info';
+import Average from './Average';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-      <Counter />
-    </div>
-  );
+  return <Average />;
 };
 
 export default App;
